4fa9feb22d27ea523431f9c8a15b799f5a749a31
Abhay Pratap Yadav <codingcontestabhay@gmail.com>
2025-12-02 11:42:45 +0530

    Update API_BASE_URL to use VITE_API_URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,1 @@
-export const API_BASE_URL = import.meta.env.PROD
-    ? "https://eventease-yxw8.onrender.com"
-    : "http://localhost:3000";
-
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
